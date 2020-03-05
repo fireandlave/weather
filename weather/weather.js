1bb7c6c55df99de0c7a16c6a248222f88147cedc
fireandlave <54378626+fireandlave@users.noreply.github.com>
2020-03-05 11:34:39 -0500

    Add files via upload

---
+++ weather/weather.js
@@ -0,0 +1,15 @@
+var highTemperatures = [55, 57, 53, 47, 56, 50];
+var lowTemperatures = [33, 30, 29, 35, 31, 32];
+
+console.log(highTemperatures[0]);
+console.log(lowTemperatures[5]);
+console.log("53");
+console.log("26.5");
+
+var avergehighTemperatures=(highTemperatures[0] + highTemperatures[1] + highTemperatures[2] + highTemperatures[3] + highTemperatures[4] + highTemperatures [5] / 6);
+
+var avergelowtemperatures=(lowTemperatures[0] + lowTemperatures[1] + lowTemperatures[2] + lowTemperatures[3] + lowTemperatures [4] + lowTemperatures [5] / 6);
+
+console.log ("its 64 degrees F and sunny");
+
+alert("weather, report");
